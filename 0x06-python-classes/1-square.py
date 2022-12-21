5629ff18eb1a81022eb526f1e1060740cf9ee94d
Adeoye <graceadeoye20@gmail.com>
2022-12-21 23:40:49 +0000

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"""Defines a class Square"""
 
 
 class Square:
-    """Represent a square."""
+    """
+    A square class with a private object attribute.
+    Check the type of the argument and raises an exception on error
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
-    def __init__(self, size):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
         self.__size = size
